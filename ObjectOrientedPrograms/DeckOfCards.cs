c681504ab3a6053c52ebba0de11be80c3358e9e8
rishiraj007 <rishiraj@mounttalent.com>
2023-04-01 15:45:04 +0530

    [Prabhu]Add: Deck of Cards

---
+++ ObjectOrientedPrograms/DeckOfCards.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPrograms
+{
+    public class DeckOfCards
+    {
+        private static readonly string[] SUITS = { "Clubs", "Diamonds", "Hearts", "Spades" };
+        private static readonly string[] RANKS = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+        private static readonly int NUM_CARDS = SUITS.Length * RANKS.Length;
+        string[] deck = new string[NUM_CARDS];
+        string[,] players = new string[4, 9];
+
+
+        // Initialize deck of cards
+        public void Card()
+        {
+            for (int i = 0; i < RANKS.Length; i++)
+            {
+                for (int j = 0; j < SUITS.Length; j++)
+                {
+                    deck[SUITS.Length * i + j] = RANKS[i] + " of " + SUITS[j];
+                }
+            }
+        }
+
+        // Shuffle the deck using Random method
+        public void Shuffle()
+        {
+            Random rnd = new Random();
+            for (int i = NUM_CARDS - 1; i > 0; i--)
+            {
+                int index = rnd.Next(i + 1);
+                string temp = deck[index];
+                deck[index] = deck[i];
+                deck[i] = temp;
+            }
+        }
+
+        // Distribute 9 cards to 4 players
+        public void Distribute()
+        {
+            
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    players[i, j] = deck[i * 9 + j];
+                }
+            }
+        }
+
+        // Print the cards received by each player using a 2D array
+        public void Display()
+        { 
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("\nPlayer " + (i + 1) + " cards:");
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.WriteLine(players[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- ObjectOrientedPrograms/Program.cs
+++ ObjectOrientedPrograms/Program.cs
@@ -1,10 +1,30 @@
 ﻿namespace ObjectOrientedPrograms
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Object Oriented Programs");
+            DeckOfCards deckOfCards = new DeckOfCards();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nEnter your choice \n1.Deck of Cards \n2.Exit");
+                Console.WriteLine("Enter option to execute");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        deckOfCards.Card();
+                        deckOfCards.Shuffle();
+                        deckOfCards.Distribute();
+                        deckOfCards.Display();
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
