e4b54f6dccbc9ba63be35d6ec16bcee094f2a1af
rishiraj007 <rishiraj@mounttalent.com>
2023-04-01 15:27:50 +0530

    [Prabhu]Add: Object Oriented Programs

---
+++ ObjectOrientedPrograms/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ObjectOrientedPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Object Oriented Programs");
+        }
+    }
+}
